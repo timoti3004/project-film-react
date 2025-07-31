3c5475376b5934249bdf903553f00f366f0b0356
HQ\900875 <timotiusginting3@gmail.com>
2025-07-31 15:43:07 +0700

    Fix: hide api key for production

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_KEY = import.meta.env.VITE_TMDB_API_KEY; 
+const API_KEY = process.env.REACT_APP_MY_API_KEY; 
 const BASE_URL = "https://api.themoviedb.org/3";
 
 export const getPopularMovies = async () => {
