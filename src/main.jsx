3a5478ccc46721f69086e71f39082b77ca733f06
HQ\900875 <timotiusginting3@gmail.com>
2025-08-04 14:59:51 +0700

    add : change loading skeleton home color

--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,19 @@ import './css/Index.css';
 import App from './App.jsx';
 import { Toaster } from 'react-hot-toast';
 import 'react-loading-skeleton/dist/skeleton.css';
+import { SkeletonTheme } from 'react-loading-skeleton';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <Toaster 
-        position="top-center" 
-        reverseOrder={false}
-      />
-      
-      <App />
-    </BrowserRouter>
+    <SkeletonTheme baseColor='#202020' highlightColor='#444'>
+      <BrowserRouter>
+        <Toaster 
+          position="top-center" 
+          reverseOrder={false}
+        />
+
+        <App />
+      </BrowserRouter>
+    </SkeletonTheme>
   </StrictMode>,
 );
